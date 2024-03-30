ec626d924c80942196aa9bb44f570c1ab44f5d26
Aditya Bhattad <adityabhattad18@gmail.com>
2024-03-30 19:14:25 +0530

    setup ai agent

--- actions/get-chain-info.ts
+++ actions/get-chain-info.ts
@@ -2,6 +2,17 @@
 import * as z from "zod";
 import { getChainInfoSchema } from "@/actions-schema";
 import { GearApi } from '@gear-js/api';
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import {pull} from ""
+
+
+
+const chat = new ChatOpenAI(
+	{ temperature: 0, openAIApiKey: env.OPENAI_KEY },
+	{ basePath: 'https://proxy.your-server.com/v1' }
+);
+
 
 
 
@@ -12,3 +23,4 @@ export async function getChainInfo(input:z.infer<typeof getChainInfoSchema>){
     const chain = await gearApi.chain();
     return {message: "success"}
 }
+
