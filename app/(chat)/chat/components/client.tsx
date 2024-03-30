1a46cb123eb00f3660e88f8a911932d561cef131
Aditya Bhattad <adityabhattad18@gmail.com>
2024-03-30 23:39:54 +0530

    add vercel generative ui component

--- app/(chat)/chat/components/client.tsx
+++ app/(chat)/chat/components/client.tsx
@@ -2,11 +2,14 @@
 import ChatHeader from "@/components/chat-header";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
-import { getChainInfo } from "@/actions/get-chain-info";
 import ChatForm from "@/components/chat-form";
 import ChatMessages from "@/components/chat-messages";
 import { ChatMessageProps } from "@/components/chat-message";
-
+import { useUIState, useActions } from "ai/rsc";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SendHorizonal } from "lucide-react";
+import type { AI } from "../../../action";
 
 
 interface ChatClientProps {
@@ -15,53 +18,78 @@ interface ChatClientProps {
 
 export default function ChatClient({ }: ChatClientProps) {
 
-    const router = useRouter();
-    const [messages, setMessages] = useState<ChatMessageProps[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [input, setInput] = useState("");
+    const [messages, setMessages] = useUIState<typeof AI>();
+    const { submitUserMessage } = useActions<typeof AI>();
 
     function handleInputChange(e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) {
         setInput(e.target.value)
     }
 
-    function onSubmit(e: FormEvent<HTMLFormElement>) {
+    async function onSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        if (input.trim() === "") return;
         setIsLoading(true);
-        const userMessage: ChatMessageProps = {
-            role: "user",
-            content: input
-        }
-        setMessages((current) => [...current, userMessage])
-        getChainInfo({userQuery:input})
-            .then((response) => {
-                const botMessage: ChatMessageProps = {
-                    role: "system",
-                    content: response.message
-                }
-                setMessages((current) => [...current, botMessage])
-                setInput("");
-                setIsLoading(false);
-            })
-            .catch((error)=>{
-                console.error(error)
-            })
+        // @ts-ignore
+        setMessages((currentMessages) => [
+            ...currentMessages,
+            {
+                id: Date.now(),
+                display: <div>{input}</div>,
+            },
+        ]);
+
+        const responseMessage = await submitUserMessage(input);
+        // @ts-ignore
+        setMessages((currentMessages) => [
+            ...currentMessages,
+            responseMessage,
+        ]);
+        setIsLoading(false);
+        setInput('');
+
     }
 
 
     return (
         <div className="flex flex-col h-full w-full">
             <ChatHeader />
-            <ChatMessages
+            {/* <ChatMessages
                 isLoading={isLoading}
                 messages={messages}
+            /> */}
+            {
+                // View messages in UI state
+                // @ts-ignore
+                messages.map((message) => (
+                    <div key={message.id}>
+                        {message.display}
+                    </div>
+                ))
+            }
+            <form
+            onSubmit={onSubmit}
+            className="border-t border-primary/10 py-4 flex items-center gap-x-2 px-4"
+            >
+            <Input
+                disabled={isLoading}
+                value={input}
+                onChange={handleInputChange}
+                placeholder="Type your message"
+                className="rounded-lg bg-primary/10"
             />
-            <ChatForm
+            <Button disabled={isLoading} variant={"ghost"} size={"icon"}>
+                <SendHorizonal
+                    className="h-4 w-4"
+                />
+            </Button>
+        </form>
+            {/* <ChatForm
                 isLoading={isLoading}
                 input={input}
                 handleInputChange={handleInputChange}
                 handleOnSubmit={onSubmit}
-            />
+            /> */}
         </div>
     )
 }
--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,28 +1,20 @@
-"use client";
+import { AI } from "@/app/action";
 import { MobileHeader } from "@/components/mobile-header";
 import Sidebar from "@/components/sidebar";
-import React, { useEffect, useState } from "react";
 
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
 
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) {
-        return null;
-    }
-
+   
     return (
-        <body>
+        <body suppressHydrationWarning>
             <MobileHeader />
             <Sidebar className="hidden lg:flex" />
             <div className="lg:pl-80 h-full pt-[50px] lg:pt-0">
                 <div className="w-full mx-auto pt-6 h-full">
-                    {children}
+                    <AI>
+                        {children}
+                    </AI>
                 </div>
             </div>
         </body>
